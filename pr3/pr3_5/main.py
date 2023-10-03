81b757abdf56e0a490ca51cbcb7b421b3479711b
Alexey Kalashnikov <kalashnickov.alexey2015@yandex.ru>
2023-10-03 17:46:01 +0300

    pr3_5-6

--- pr3/pr3_5/main.py
+++ pr3/pr3_5/main.py
@@ -6,12 +6,20 @@ from scipy import rand
 
 def make_box_plot(data):
     plt.figure(figsize=(8, 8))
-    plt.boxplot(x=[
-        data["age"],
-        data["bmi"],
-        data["children"],
-        # data["charges"]
-    ], vert=False, labels=["Age", "BMI", "Children"])
+    # plt.boxplot(x=[
+    #     data["age"],
+    #     data["bmi"],
+    #     data["children"],
+    #     # data["charges"]
+    # ], vert=False, labels=["Age", "BMI", "Children"])
+    plt.subplot(411)
+    plt.boxplot(data["age"], labels=["Age"], vert=False)
+    plt.subplot(412)
+    plt.boxplot(data["bmi"], labels=["BMI"], vert=False)
+    plt.subplot(413)
+    plt.boxplot(data["children"], labels=["Children"], vert=False)
+    plt.subplot(414)
+    plt.boxplot(data["charges"], labels=["Charges"], vert=False)
     plt.xticks(np.arange(0, 105, 5))
     # plt.scatter(data["age"], rand, s=1.5)
     plt.grid()
---
+++ pr3/pr3_6/main.py
@@ -0,0 +1,35 @@
+from random import randrange
+
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+
+def cpt(data, l, q):
+    vals = list()
+    for i in range(q):
+        temp = list()
+        for i in range(l):
+            temp.append(data[randrange(0, len(data) - 1, 1)])
+        vals.append(sum(temp) / len(temp))
+    return vals
+
+
+def make_hist(data, title, avg):
+    plt.hist(data)
+    plt.title(title)
+    plt.axvline(x=avg, color="black", label="Среднее")
+    plt.legend()
+    plt.show()
+
+
+if __name__ == '__main__':
+    data = pd.read_csv("../insurance.csv", sep=',')
+
+    for l in (1,5,20,50):
+        res = pd.Series(cpt(data["charges"], l, 300))
+        std = res.std()
+
+        avg = np.mean(res)
+        print(f"Стандартное отклонение: {std}, Среднее арифметическое: {avg}")
+        make_hist(res, f"Длина выборки: {l}", avg)

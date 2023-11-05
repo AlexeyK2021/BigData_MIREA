f397c0085b5210a1abf6329d879d778635c217ff
Kalashnikov Alexey <kalashnickov.alexey2015@yandex.ru>
2023-11-05 15:28:39 +0300

    pr4_2

--- pr4/pr4_2/main.py
+++ pr4/pr4_2/main.py
@@ -5,34 +5,42 @@ import seaborn as sns
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
-
-def mserror(x, w1, w0, y):
-    ypred = w1 * x[:, 0] + w0
-    return np.sum((y - ypred) ** 2) / len(ypred)
-
-
-def gr_mserror(x, w1, w0, y):
-    ypred = w1 * x[:, 0] + w0
-    return np.array([2 / len(x) * np.sum((y - ypred)) * (-1)])
-
-
-def regression():
-    epc = 0.0001
-    w1 = 0
-    w0 = 0
-
-    learning_rate = 0.001
-    next_w1 = w1
-    next_w0 = w0
-
-    n = 100000
-
-    for i in range(n):
-        cur_w1 = next_w1
-        cur_w0 = next_w0
-
-        next_w0 = cur_w0 - learning_rate * gr_mserror()
-
+#
+# def mserror(x, w1, w0, y):
+#     ypred = w1 * x[:, 0] + w0
+#     return np.sum((y - ypred) ** 2) / len(ypred)
+#
+#
+# def gr_mserror(x, w1, w0, y):
+#     ypred = w1 * x[:, 0] + w0
+#     return np.array([2 / len(x) * np.sum((y - ypred)) * (-1)])
+#
+#
+# def regression(x, y):
+#     eps = 0.0001
+#     w1 = 0
+#     w0 = 0
+#
+#     learning_rate = 0.001
+#     next_w1 = w1
+#     next_w0 = w0
+#
+#     n = 100000
+#
+#     for i in range(n):
+#         cur_w1 = next_w1
+#         cur_w0 = next_w0
+#         err = gr_mserror(x, cur_w1, cur_w0, y)
+#         next_w0 = cur_w0 - learning_rate * err[0]
+#         next_w1 = cur_w1 - learning_rate * err[0]
+#         print(f"Итерация {i}:")
+#         print(f"Текущая точка {cur_w1, cur_w0}| Следущая точка {next_w1, next_w0}")
+#         print(f"MSE = {mserror(x, cur_w1, cur_w0, y)}")
+#         print("--------------------------------------------------------------------")
+#
+#         if (abs(cur_w1 - next_w1) <= eps) and (abs(cur_w0 - next_w0) <= eps):
+#             break
+#
 
 if __name__ == '__main__':
     data = pd.read_csv("../smartphones.csv", sep=",")
@@ -69,5 +77,3 @@ if __name__ == '__main__':
     plt.show()
     mse = mean_squared_error(y, y_func)
     print(f"MSE = {mse}")
-
-    # regression()
